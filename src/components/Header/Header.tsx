f3fd923590a17970d75316f50d498af3869f6f0a
Shorya <shoryadhyani@gmail.com>
2025-06-28 11:41:07 +0530

    -n

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -40,7 +40,7 @@ export function Header() {
           <NavHashLink smooth to="#contact" onClick={closeMenu}>
             Contact
           </NavHashLink>
-          <a href={Resume} download className="button">
+          <a download className="button">
             Resume
           </a>
         </nav>
